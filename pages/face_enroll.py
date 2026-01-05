eb2fcd63c7ca3aafd40a26d4a25ceb58a80e9477
Keyur Solanki <166228687+keyur-18@users.noreply.github.com>
2026-01-05 11:49:32 +0530

    face page is added

---
+++ pages/face_enroll.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from PIL import Image
+import os
+import io
+from datetime import datetime
+
+st.set_page_config(page_title="Face enroll",layout="centered")
+st.title("face enrollment")
+st.write("Capture **3 face images** from different angles")
+
+
+username = st.text_input("enter usernamee")
+if not username:
+    st.warning("Please enter username to continue")
+    st.stop()
+st.subheader("capure images")
+
+img_front = st.camera_input("Front face")
+img_left  = st.camera_input("Left Angle")
+img_right = st.camera_input("Right Angle")
+print(type(img_front))
+images = {
+    "front":img_front,
+    "left":img_left,
+    "right":img_right
+}
+
+
+if st.button("save images"):
+    if None in images.values():
+        st.error("please capure images")
+    else:
+        user_dir = f"../images/{username}"
+        os.makedirs(user_dir,exist_ok=True)
+
+        for angle,img in images.items():
+            image = Image.open(img)
+            image.save(f"{user_dir}/{angle}.jpg")
+        st.success("images saved")
+        st.balloons()
