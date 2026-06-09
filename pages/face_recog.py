9d1ef95747a704595d7450a698a38ae36153b4b8
Keyur Solanki <166228687+keyur-18@users.noreply.github.com>
2026-06-09 09:26:58 +0530

    yolo and arcface introduced

--- pages/face_recog.py
+++ pages/face_recog.py
@@ -1,8 +1,11 @@
 import streamlit as st
-from deepface import DeepFace
+# from deepface import DeepFace
+from PIL import Image
+import numpy as np
 from scipy.spatial.distance import cosine
 from utils.embed import save_embeddings,recognition
-
+from ultralytics  import YOLO
+model = YOLO(r'model\yolov8n-face.pt')
 st.set_page_config(page_title="Face Recognition")
 st.title("Face Recognition")
 img=st.camera_input("capture your image")
@@ -12,8 +15,17 @@ generate = st.button("Generate")
 
 if generate:
     if img is not None:
-        name = recognition(img)
-        st.success(f"Recognized as: {name}")
+        ans = []
+        img = Image.open(img)
+        img = np.array(img)
+        results = model.track(img)
+        boxes = results[0].boxes
+        for box in boxes:
+            x1,y1,x2,y2 = map(int,box.xyxy[0])
+            face = img[y1:y2,x1:x2]
+            name = recognition(face)
+            ans.append(name)
+        st.success(f"Recognized as: {ans}")
     else:
         st.warning("Please capture an image")
     st.balloons()
